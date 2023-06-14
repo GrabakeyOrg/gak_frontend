33f2f0ab72ef8f7b2f134c99dd3538daec9351ee
Samuel Ventura <samuel.ventura@yeico.com>
2023-06-14 00:34:51 -0600

    Basic branding.

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,29 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import grabakeyLogo from "/logo.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    <a href="https://grabakey.org" target="_blank" rel="noreferrer">
+      <img src={grabakeyLogo} class="logo" alt="Grabakey Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Grabakey</h1>
 
   <div class="card">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
@@ -36,10 +34,7 @@
     transition: filter 300ms;
   }
   .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+    filter: drop-shadow(0 0 2em #12b09baa);
   }
   .read-the-docs {
     color: #888;
